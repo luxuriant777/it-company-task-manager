a49e2f56aa6497a6cca117167d486ed5e35a923d
Oleksandr Kolomoiets <0985305543@ukr.net>
2022-11-10 13:57:11 +0200

    Added 'index' to views.py for task_manager app

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from task_manager.models import Worker, Task
 
-# Create your views here.
+
+def index(request):
+    num_workers = Worker.objects.count()
+    num_tasks = Task.objects.count()
+
+    num_visits = request.session.get("num_visits", 1)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_workers": num_workers,
+        "num_tasks": num_tasks,
+        "num_visits": num_visits,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
