13d9f04fbbb14437b63f9b4a7bfcc863a5dd3d0f
Oleksandr Kolomoiets <0985305543@ukr.net>
2022-11-10 13:30:13 +0200

    Changed Meta options on task and worker models, altered priority on task, added admin.py model

--- task_manager/models.py
+++ task_manager/models.py
@@ -5,30 +5,45 @@ from django.db import models
 class TaskType(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Position(models.Model):
     name = models.CharField(max_length=100)
 
-
+    def __str__(self) -> str:
+        return self.name
 class Worker(AbstractUser):
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
 
 class Task(models.Model):
+    class Meta:
+        ordering = ["priority"]
+
     name = models.CharField(max_length=100)
     description = models.TextField()
     deadline = models.DateField()
     is_completed = models.BooleanField()
     PRIORITY_CHOICES = [
-        ("urgent", "Urgent"),
-        ("high", "High"),
-        ("medium", "Medium"),
-        ("low", "Low"),
+        (1, "Urgent"),
+        (2, "High"),
+        (3, "Medium"),
+        (4, "Low"),
     ]
-    priority = models.CharField(
-        max_length=6,
+    priority = models.IntegerField(
         choices=PRIORITY_CHOICES,
         default="low"
     )
     task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
     assignees = models.ManyToManyField(Worker, related_name="tasks")
+
+    def __str__(self) -> str:
+        return f"{self.name} (Priority: {self.priority})"
