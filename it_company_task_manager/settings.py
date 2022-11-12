bcc9f3430bbeb95a2629a197bd5b13e06396e2e2
Oleksandr Kolomoiets <0985305543@ukr.net>
2022-11-12 20:09:50 +0200

    Some settings and requirements were modified

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,6 +80,11 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+DATABASES["default"].update(db_from_env)
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
