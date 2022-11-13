391bb33d5e88f3e1ecba103d20deadc462023e15
Oleksandr Kolomoiets <0985305543@ukr.net>
2022-11-13 02:06:40 +0200

    Added managing of static files

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -27,7 +27,7 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles/"
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
